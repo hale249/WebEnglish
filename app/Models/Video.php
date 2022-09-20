cf6661061fc8114b5824726a357b40fca523e854
hale249 <lehatybg1@gmail.com>
2022-09-20 10:47:23 +0700

    Added relationship video

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Video extends Model
 {
     protected $table = 'videos';
 
     protected $guarded = [];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
 }
